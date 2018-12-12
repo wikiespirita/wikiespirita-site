89f1d4750f97dcef06eb9b3f7999c33a626ad037
Evert Ramos <evert.ramos@gmail.com>
2018-12-12 17:12:51 -0200

    teste circleci t18

--- deploy.sh
+++ deploy.sh
@@ -38,7 +38,8 @@ git add -A
 git commit -m "[$LAST_COMMIT_MESSAGE] Deploy by CI"
 git checkout -b gh-pages
 
-git push -f --set-upstream $(git config --get remote.origin.url):gh-pages gh-pages
+#git push -f --set-upstream $(git config --get remote.origin.url) gh-pages
+git push -f $(git config --get remote.origin.url) gh-pages
 #git push -f --set-upstream $(git config --get remote.origin.url) gh-pages:gh-pages
 #git push -f $(git config --get remote.origin.url) gh-pages:gh-pages
 #git push -f $(git config --get remote.origin.url) master:master
