6d55d5483c951dc54c17f59d16ee55f761e2d5ad
Evert Ramos <evert.ramos@gmail.com>
2018-12-12 15:13:58 -0200

    teste circleci t6

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,24 @@
 #!/bin/bash -e
 
-# build
+# Exit on Error
+set -e
+
+# Local Variables
+local DIST_FOLDER=site
+
+# Save the Commit Message
+local LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+
+# Prepare the environment
+git worktree add $DIST_FOLDER gh-pages
+
+# Save the '.git' file
+mv ./$DIST_FOLDER/.git .git-$DIST_FOLDER
+
+# Remove worktree folder
+rm -rf ./$DIST_FOLDER
+
+# Build Application
 #yarn run --silent build
 npm run build
 
@@ -9,18 +27,14 @@ if [ "$(git status --porcelain | wc -l | xargs)" -eq 0 ]; then
   exit 0
 fi
 
-cd site
+cd $DIST_FOLDER
 echo 'wikiespirita.com.br' > CNAME
 
 git config --global user.name "Circle CI"
 git config --global user.email "<>"
 git init
 git add -A
-git commit -m "[ci skip] Deploy by CI"
-
-echo "everything is ok up to here"
-git remote -v
-git status
+git commit -m "[$LAST_COMMIT_MESSAGE] Deploy by CI"
 
 git push -f $(git config --get remote.origin.url) gh-pages:gh-pages
 #git push -f $(git config --get remote.origin.url) master:master
